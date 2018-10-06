4b594b702f1461c8a518cd1654d2d7d20ac22eed
Demian Florentin <demian@synarchys.com>
2018-10-06 10:02:17 -0400

    contact added

--- src/components/sections.nim
+++ src/components/sections.nim
@@ -23,7 +23,9 @@ proc Card(def: JsonNode): VNode =
             text pt.getStr()
             if def.hasKey("link"):
               let link = def["link"]
-              a(href=link["href"].getStr, class="mb-auto card-link btn btn-lg btn-block btn-primary"): text link["text"].getStr
+              a(href=link["href"].getStr,
+                class="mb-auto card-link btn btn-lg btn-block btn-primary"):
+                text link["text"].getStr
           
 proc Cards(def: JsonNode): VNode =
   result = buildHtml(tdiv(class="container")):
@@ -33,6 +35,25 @@ proc Cards(def: JsonNode): VNode =
         if child.hasKey("card"):
           Card(child["card"])
 
+
+# proc contact(contacts: JsonNode): VNode =
+#   result = buildHtml(section(class="bg-white", id="contact")):
+#     tdiv(class="container"):
+#       tdiv(class="row"):
+#         tdiv(class="col text-center"):
+#           h2(class="section-heading text-center"): text "Contact"
+#       h3(class="section-subheading text-muted")
+#       tdiv(class="text-justify text-black"):
+#         tdiv(class="container"):
+#           tdiv(class="row"):
+#             for c in contacts["items"]:
+#               tdiv(class="col text-justify"):
+#                 h2: text c["name"].getStr()
+#                 for l in c["address_lines"]:
+#                   p: text l.getStr()
+#                 p:
+#                   a(href=c["email"].getStr()): text c["email"].getStr()
+          
 proc Sections*(def: JsonNode): VNode =
   let sections  = def["container"]["sections"]
   result = buildHtml(tdiv()):
@@ -43,3 +64,14 @@ proc Sections*(def: JsonNode): VNode =
             Paragraph(sect)
         elif sect["type"].getStr == "cards":
           Cards(sect)
+        elif sect["type"].getStr == "rows":
+          # tdiv(class="container"):
+          #   h2(class="section-heading text-center"): text(sect["title"].getStr())
+          #   tdiv(class="row justify-content-center align-items-start")
+          tdiv(class="container"):
+            tdiv(class="row"):
+              tdiv(class="col text-center"):
+                h2(class="section-heading text-center"):
+                  text sect["title"].getStr()
+            #Paragraph(sect)
+            
