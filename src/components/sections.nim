8fb454aa7988912e80d6b0fa405bf756c989505d
Demian Florentin <demian@synarchys.com>
2018-10-06 03:59:32 -0400

    cards aligned

--- src/components/sections.nim
+++ src/components/sections.nim
@@ -4,45 +4,42 @@ import karax / [prelude, vstyles]
 
 import sugar, json
 
-# TODO: move section header somewhere from the components
-
 proc Paragraph(def: JsonNode): VNode =
-  echo $def
   result = buildHtml(tdiv(class="col-md text-justify")):
-    h2(class="section-heading text-justify"): text(def["title"].getStr())
+    h2(class="section-heading text-center"): text(def["title"].getStr())
     for child in def["children"]:
       if child.hasKey("p"):
         for pa in child["p"]:
           p: text pa.getStr()
 
 proc Card(def: JsonNode): VNode =
-  result = buildHtml(tdiv(class="row")):
-    h2(class="section-heading text-justify"): text(def["title"].getStr())
-    tdiv(class="row"):
+  result = buildHtml(tdiv(class="col")):
+    tdiv(class="card mb-4 box-shadow products-card"):
+      h5(class="card-header"):
+        text def["title"].getStr()
+      tdiv(class="card-body d-flex flex-column"):
+        p(class="card-text"):
+          for pt in def["content"]:
+            text pt.getStr()
+            if def.hasKey("link"):
+              let link = def["link"]
+              a(href=link["href"].getStr, class="mb-auto card-link btn btn-lg btn-block btn-primary"): text link["text"].getStr
+          
+proc Cards(def: JsonNode): VNode =
+  result = buildHtml(tdiv(class="container")):
+    h2(class="section-heading text-center"): text(def["title"].getStr())
+    tdiv(class="row justify-content-center align-items-start"):
       for child in def["children"]:
         if child.hasKey("card"):
-          let c = child["card"]
-          tdiv(class="card", style= style(StyleAttr.width, "33rem")):
-            img(class="card-img-top", src="...",alt="Card image cap")
-            tdiv(class="card-body"):
-              h5(class="card-title"): text c["title"].getStr()
-              p(class="card-text"):
-                for pt in c["content"]:
-                  text pt.getStr()
-                if c.hasKey("link"):
-                  let link = c["link"]
-                  tdiv():
-                    a(href=link["href"].getStr, class="btn btn-primary"): text link["text"].getStr
-
+          Card(child["card"])
 
 proc Sections*(def: JsonNode): VNode =
   let sections  = def["container"]["sections"]
   result = buildHtml(tdiv()):
     for sect in sections:
       section(class="bg-white", id=sect["id"].getStr()):
-        tdiv(class="container"):
-          if sect["type"].getStr == "text":          
-            tdiv(class="row"):
-              Paragraph(sect)
-          elif sect["type"].getStr == "cards":
-            Card(sect)
+        if sect["type"].getStr == "text":
+          tdiv(class="container"):
+            Paragraph(sect)
+        elif sect["type"].getStr == "cards":
+          Cards(sect)
